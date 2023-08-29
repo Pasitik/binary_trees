a9e5280a11bb4682c9419192f6c64c721c10816d
DD1C74707 <gracevalley101@gmail.com>
2023-08-29 12:51:41 -0700

    Update task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,20 +10,23 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if(parent == NULL)
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-	if(new_node == NULL)
+	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
 		return (NULL);
 
-	if(parent->right == NULL)
+	if (parent->right == NULL)
 	{
 		parent->right = new_node;
 	}
 	else
 	{
-		new_node->right = parent->right;  
+		new_node->right = parent->right;
 		parent->right->parent = new_node;
 		parent->right = new_node;
 	}
