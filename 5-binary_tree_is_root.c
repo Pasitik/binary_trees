07e056e53c4a66adef0995c5544a62e929e2b51c
DD1C74707 <gracevalley101@gmail.com>
2023-08-29 13:03:31 -0700

    Fix betty for tsk 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,10 +8,10 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if(node == NULL)
+	if (node == NULL)
 		return (0);
 
-	if(node->left != NULL && node->right != NULL)
+	if (node->left != NULL && node->right != NULL)
 		return (1);
 
 	return (0);
