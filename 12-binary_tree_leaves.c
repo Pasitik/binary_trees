a2b76aa293610dd80f13603ab7862b3189af54db
DD1C74707 <gracevalley101@gmail.com>
2023-08-29 14:26:14 -0700

    Update task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,11 +9,12 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
+
 	if (tree)
 	{
 		leaves += (tree->right || tree->left) ? 0 : 1;
 		leaves += binary_tree_leaves(tree->left);
 		leaves += binary_tree_leaves(tree->right);
 	}
-	return leaves;
+	return (leaves);
 }
