38ed30f5d7790755c1625b19d4e27e346ba691a9
Paul Asitik <paasitik@gmail.com>
2023-08-29 12:06:16 -0700

    12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - measures the leaves of a binary tree
+ * @tree: pointer to binary_tree_t
+ *
+ * Return: size_t
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+	if(tree)
+	{
+		leaves += (tree->right || tree->left) ? 0 : 1;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return leaves;
+}
