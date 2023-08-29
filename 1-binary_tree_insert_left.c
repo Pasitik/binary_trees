989ed878b0eabc9bc608ba017c85f4ecaccbe94a
DD1C74707 <gracevalley101@gmail.com>
2023-08-29 12:32:38 -0700

    Resolve betty for task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,20 +10,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if(parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	binary_tree_t *new_node = binary_tree_node(parent, value);
-	if(new_node == NULL)
+
+	if (new_node == NULL)
 		return (NULL);
 
-	if(parent->left == NULL)
+	if (parent->left == NULL)
 	{
 		parent->left = new_node;
 	}
 	else
 	{
-		new_node->left = parent->left;  
+		new_node->left = parent->left;
 		parent->left->parent = new_node;
 		parent->left = new_node;
 	}
