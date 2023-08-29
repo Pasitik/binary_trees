a6a28c848a7ec30805060ba84e2d5939582c130e
DD1C74707 <gracevalley101@gmail.com>
2023-08-29 14:19:18 -0700

    Resolve betty for task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
 * binary_tree_size - measures the size of a binary tree
 * @tree: pointer to binary_tree_t
@@ -8,13 +9,13 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if(tree == NULL)
-        return (0);
-        
-    size_t l, r = 0;
-    
-    l = binary_tree_size(tree->left);
-    r = binary_tree_size(tree->right);
-    
-    return (l + r + 1);
+	size_t l, r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+
+	return (l + r + 1);
 }
