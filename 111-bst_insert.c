8dbfa6498a55a862c071835e89a201ba6c2969f5
DD1C74707 <gracevalley101@gmail.com>
2023-08-31 10:03:53 -0700

    Add bst insert

---
+++ 111-bst_insert.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - Performs a left-rotation on a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to rotate
+ *
+ * Return: Pointer to the new root node after rotation
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+
+	if (value < (*tree)->n)
+	{	
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		else
+		{
+			return (bst_insert(&((*tree)->left), value));
+		}
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		else
+		{
+			return (bst_insert(&((*tree)->right), value));
+		}
+	}
+
+	return (NULL);
+}
+
