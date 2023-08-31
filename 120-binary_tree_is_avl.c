b87953d005e405d124af48cab6fcd63c3fe98df5
DD1C74707 <gracevalley101@gmail.com>
2023-08-31 13:19:15 -0700

    Resolve betty skip check

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -22,7 +22,8 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 	if ((tree->left != NULL && tree->left->n >= tree->n) ||
 		(tree->right != NULL && tree->right->n <= tree->n))
 		return (0);
-	return (binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right));
+	return (binary_tree_is_bst(tree->left)
+			&& binary_tree_is_bst(tree->right));
 }
 
 /**
