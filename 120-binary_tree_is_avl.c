e42000584163bba5751f70db0e615043f6e84769
DD1C74707 <gracevalley101@gmail.com>
2023-08-31 13:12:08 -0700

    Update task 120

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -17,9 +17,11 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 	balance = lheight - rheight;
 	if (balance < -1 || balance > 1)
 		return (0);
-	if (abs(balance) != 1 && !binary_tree_is_bst(tree))
+	if (!binary_tree_is_bst(tree))
 		return (0);
-
+	if ((tree->left != NULL && tree->left->n >= tree->n) ||
+		(tree->right != NULL && tree->right->n <= tree->n))
+		return (0);
 	return (binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right));
 }
 
