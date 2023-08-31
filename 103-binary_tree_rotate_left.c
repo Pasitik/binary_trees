cc23c2cdcb7fb281bbe32a46bed5d336a9ae5e1c
DD1C74707 <gracevalley101@gmail.com>
2023-08-31 09:07:47 -0700

    Add left rotation

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,66 @@
+#include "binary_trees.h"
+
+/**
+ * max_ - returns the max of numbers
+ * @a: first number
+ * @b: second number
+ * Return: the max of a and b
+ */
+int max_(int a, int b)
+{
+	return (a > b ? a : b);
+}
+
+/**
+ * avl_height - Performs a left-rotation on a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to rotate
+ *
+ * Return: Pointer to the new root node after rotation
+ */
+int avl_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+
+	return (1 + max_(avl_height(tree->left), avl_height(tree->right)));
+}
+
+/**
+ * binary_tree_rotate_left - Performs a left-rotation on a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to rotate
+ *
+ * Return: Pointer to the new root node after rotation
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *temp, *new_root;
+
+	temp = NULL;
+	if (tree == NULL)
+		return (NULL);
+	if (tree->right->left != NULL)
+		temp = tree->right->left;
+	if (tree->right == NULL)
+		return (tree);
+	new_root = tree->right;
+	tree->right->left = tree;
+	tree->parent = tree->right;
+	tree->right->parent = NULL;
+	tree->right = temp;
+	if (temp != NULL)
+		temp->parent = tree;
+
+	new_root->height = max_(avl_height(new_root->left),
+			avl_height(new_root->right)) + 1;
+	/**
+	 * temp->height = max_(avl_height(temp->left),
+	 *  avl_height(temp->right)) + 1;
+	 */
+	tree->height = max_(avl_height(tree->left),
+			avl_height(tree->right)) + 1;
+
+	return (new_root);
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -59,6 +59,7 @@ void levelordering(const binary_tree_t *tree, void (*func)(int), int height);
 int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_complete_recursive(const binary_tree_t *tree,
                 size_t index, size_t node_count);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 
 #endif
