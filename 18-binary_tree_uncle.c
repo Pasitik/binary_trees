302716ba6494efe144c358ed23c910abeaa824e6
DD1C74707 <gracevalley101@gmail.com>
2023-08-31 03:03:27 -0700

    Update task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-
+	if (node->parent == NULL)
+		return (NULL);
 	if (node->parent->parent == NULL)
 		return (0);
 
